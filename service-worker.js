19631087ef0b961a7c3cbce575b04d267a06b13e
daplevat67-coder <daplevat67@gmail.com>
2026-04-26 10:53:22 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'coffee-zerno-v1';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).then(res => {
+    const clone = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+    return res;
+  }).catch(() => caches.match(e.request)));
+});
