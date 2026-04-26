ffb5e211252f12234c30b44e290200d64b251226
daplevat67-coder <daplevat67@gmail.com>
2026-04-27 00:07:01 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('coffee-v1').then(cache => {
-    return cache.addAll(['./', './index.html', './style.css', './script.js']);
-  }));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
